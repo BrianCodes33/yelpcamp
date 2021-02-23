79af6767a64b55d90d9f0daba85643906325e3e5
Brian Borin <brianborin@icloud.com>
2021-02-23 10:57:56 -0800

    added more bootstrap styling to views and layout

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'http://source.unsplash.com/collection/8510479',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam optio vero minima libero fugiat qui excepturi beatae pariatur. Beatae, labore?',
+            price
         })
         await camp.save();
     }
