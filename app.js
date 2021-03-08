bac041b8594323e021ef4b5be402bf7c75206720
Brian Borin <brianborin@icloud.com>
2021-03-08 09:20:52 -0800

    refactor Joi middleware

--- app.js
+++ app.js
@@ -29,56 +29,65 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(morgan('combined'))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
+
+const validateCampground = (req, res, next) => {
+  const campgroundSchema = Joi.object({
+    campground: Joi.object({
+      title: Joi.string().required(),
+      price: Joi.number().required().min(0),
+      image: Joi.string().required(),
+      location: Joi.string().required(),
+      description: Joi.string().required()
+    }).required()
+  })
+  const { error } = campgroundSchema.validate(req.body)
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next()
+  }
+}
 
 app.get('/', (req, res) => {
   res.render('home')
-  // throw new Error('error')
 })
 
 app.get('/campgrounds', catchAsync(async (req, res) => {
   const campgrounds = await Campground.find({})
-  res.render('campgrounds/index', { campgrounds })
+  res.render('campgrounds/index', {
+    campgrounds
+  })
 }))
 
 app.get('/campgrounds/new', async (req, res) => {
   res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', catchAsync(async(req, res, next) => {
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-        title: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
-      }).required()
-    })
-    const { error } = campgroundSchema.validate(req.body)
-    if (error) {
-      const msg = error.details.map(el => el.message).join(',')
-      throw new ExpressError(msg, 400)
-    }
-    
-    const campground = new Campground(req.body.campground)
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+  const campground = new Campground(req.body.campground)
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id)
   console.log(campground)
-  res.render('campgrounds/show', { campground })
+  res.render('campgrounds/show', {
+    campground
+  })
 }))
 
-app.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id)
-  res.render('campgrounds/edit', { campground })
+  res.render('campgrounds/edit', {
+    campground
+  })
 }))
 
-app.put('/campgrounds/:id', catchAsync(async(req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
   res.redirect(`/campgrounds/${campground._id}`)
@@ -95,8 +104,8 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-  const {statusCode = 500} = err
-  if(!err.message) err.message = 'Oh no, something wrong'
+  const { statusCode = 500 } = err
+  if (!err.message) err.message = 'Oh no, something wrong'
   res.status(statusCode).render('error', { err })
 })
 
