c193404730f90effe15586624c91c4887f8dd40a
Brian Borin <brianborin@icloud.com>
2021-04-08 20:12:10 -0700

    adding delete review routing

--- app.js
+++ app.js
@@ -105,6 +105,10 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
   res.redirect(`/campgrounds/${campground._id}`)
 }))
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    res.send('delete me')
+}))
+
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page not found', 404))
 })
